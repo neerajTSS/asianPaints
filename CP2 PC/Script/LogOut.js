8629f96f3376c32243d22ef3670915cc56ed0b7b
Neeraj Sharma (QA) <neeraj.sharma2@technossus.com>
2024-02-09 11:26:18 +0530

    Changes into the Login, HomePage and Logout Script

--- CP2 PC/Script/LogOut.js
+++ CP2 PC/Script/LogOut.js
@@ -1,13 +1,12 @@
 ﻿function LogOut()
 {
-  //Opens the specified URL in a running instance of the specified browser.
-  //Browsers.Item(btChrome).Navigate("https://my349520.crm.ondemand.com/sap/ap/ui/clogin?saml2=disabled&app.component=/SAP_UI_CT/Main/root.uiccwoc&rootWindow=X&redirectUrl=/sap/public/byd/runtime#Nav/0/eyJfc1dvY0lkIjpudWxsLCJfc1ZpZXdJZCI6bnVsbCwiX3NQZXJmb3JtYW5jZVdvY0lkIjoiSG9tZSIsIl9zUGVyZm9ybWFuY2VWaWV3SWQiOiJIb21lIiwiX3NDb250ZXh0SWQiOiI2OTRmMTU2YWQ1MzVlN2RhZGVhN2JiZDY3YTQ3NGJkZSJ9");
-  //Maximizes the specified Window object.
   Aliases.browser.BrowserWindow.Maximize();
-  //Clicks the 'textnodeCustomer' control.
-  Aliases.browser.pageLogon.headerMainshellHdr.textnodeCustomer.Click();
-  //Clicks the 'buttonButton116' button.
-  Aliases.browser.pageLogon.footerHelp.buttonButton116.ClickButton();
-  //Closes the 'BrowserWindow' window.
+  Aliases.browser.pageLogon.headerMainshellNRJ.textnodeCustomer.Click();
+  Aliases.browser.pageLogon.headerMainshellNRJ.buttonSignOut.Click();
+  Aliases.explorer.wndShell_SecondaryTrayWnd.DesktopWindowXamlSource.Click(1212, 23);
+  aqObject.CheckProperty(Aliases.browser.pageLogon.textnodeMbox0Title, "VisibleOnScreen", cmpEqual, true);
+  Log.Message("SignOut popUp is visible on the Screen\r\n", "");
+  Aliases.browser.BrowserWindow.Maximize();
+  Aliases.browser.pageLogon.headerMainshellNRJ.buttonYes.ClickButton();
   Aliases.browser.BrowserWindow.Close();
 }
